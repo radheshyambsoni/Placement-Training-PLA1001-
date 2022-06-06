fd467612386332046777fc57bedeb5b7c6f0e49c
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-06-06 16:29:14 +0530

    Updated some files and added some missing ones

--- src/May24_weightedSubstring.java
+++ src/May24_weightedSubstring.java
@@ -2,15 +2,16 @@ public class May24_weightedSubstring {
     public static void main(String[] args) {
         String P="Hello";
         int Q[]={9,8,7,6,5};
-        int k=25;
+        int k=0;
+        weightedSubstr(P, Q, k);
+    }
 
+    static void weightedSubstr(String P, int[] Q, int k) {
+        if(k<=0){System.out.println("Not possible for limit less than or equal to zero");return;}
         for(int i=0;i<P.length();i++){
             String temp="";
-            temp+=P.charAt(i);
-            System.out.print(temp+" ");
-
-            int wt=Q[i];
-            for(int j=i+1;j<P.length();j++){
+            int wt=0;
+            for(int j=i;j<P.length();j++){
                 wt+=Q[j];
                 if(wt<=k){
                     temp+=P.charAt(j);
